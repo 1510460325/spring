2552990af5e3ddd7bcafbc81009062bfe8684d46
1510460325@qq.com <1510460325@qq.com>
2018-12-10 10:46:40 +0800

    上传图片

--- src/test/java/cn/wzy/ClassFindTest.java
+++ src/test/java/cn/wzy/ClassFindTest.java
@@ -1,9 +1,37 @@
 package cn.wzy;
 
 
+import org.springframework.beans.MutablePropertyValues;
+import org.springframework.beans.PropertyValue;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.BeanDefinitionHolder;
+import org.springframework.beans.factory.support.BeanDefinitionReader;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.core.io.Resource;
+
 public class ClassFindTest {
 
 	public static void main(String[] args) {
-		System.out.println("hello world");
+
+		//维护一个Map<beanName, definition>的ioc容器仓库。
+		DefaultListableBeanFactory factory;
+
+		//包装配置文件（如xml文件）
+		Resource resource;
+
+		//把Resource中的信息加载成Document然后再加载成Definition包装成BeanDefinitionHolder
+		BeanDefinitionReader reader;
+
+		//包装一个definition
+		BeanDefinitionHolder holder;
+
+		//定义一个bean
+		BeanDefinition definition1;
+		//维护一个PropertyValue的列表
+		MutablePropertyValues mutablePropertyValues;
+		//存储属性值得关系
+		PropertyValue propertyValue;
+
+
 	}
 }
